b4438148a97f690a5cdd669e37d3a14a616099d5
hayat <hayatalbarbarawi@gmail.com>
2025-10-09 16:35:54 +0300

    Added Simple Login and Logout

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views 
+from . import views
 
 app_name = 'main_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('skills/', views.skills_list, name='skills_list'),    
-    path('projects', views.projects_list, name='projects_list')  
+    path('projects', views.projects_list, name='projects_list'),
+    path('login/', views.user_login, name='login'),  
+    path('logout/', views.user_logout, name='logout'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 from .models import Skill, Project, Task, Reflection
 
@@ -11,3 +13,18 @@ def skills_list(request):
 
 def projects_list(request):
       return render(request, 'main_app/projects_list.html')
+
+def user_login(request):
+      if request.method == 'POST':
+          username = request.POST['username']
+          password = request.POST['password']
+          user = authenticate(request, username=username, password=password)        
+          if user is not None:
+              login(request, user)
+              return redirect('main_app:home')
+      return render(request, 'main_app/login.html')
+
+
+def user_logout(request):
+      logout(request)
+      return redirect('main_app:home')

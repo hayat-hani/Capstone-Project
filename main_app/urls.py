75a98aab10f42b4ce239f2334287d62cd6e053f9
hayat <hayatalbarbarawi@gmail.com>
2025-10-09 23:10:28 +0300

    Typo fixes

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ app_name = 'main_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('skills/', views.skills_list, name='skills_list'),    
-    path('projects', views.projects_list, name='projects_list'),
+    path('projects/', views.projects_list, name='projects_list'),
     path('login/', views.user_login, name='login'),  
     path('logout/', views.user_logout, name='logout'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Skill, Project, Task, Reflection
 
@@ -18,7 +19,7 @@ def skills_list(request):
             skills = Skill.objects.filter(user=request.user)
             return render(request, 'main_app/skills_list.html', {'skills': skills})
       else:
-            return redirect('main_app:loging')
+            return redirect('main_app:login')
 
 def projects_list(request):
       if request.user.is_authenticated:
@@ -41,3 +42,4 @@ def user_login(request):
 def user_logout(request):
       logout(request)
       return redirect('main_app:home')
+
